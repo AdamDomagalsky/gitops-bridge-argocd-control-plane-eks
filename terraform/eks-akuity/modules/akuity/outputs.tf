dc7ccdd46f0d77cdbf037da485aa082fe53af95f
Adam Domagalski <adam.domagalski1994@gmail.com>
2023-11-23 12:51:22 +0100

    remove akuity gke, eks

--- terraform/eks-akuity/modules/akuity/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "akuity_server_addr" {
-  description = "Argo CD Server hostname"
-  value       = "${akp_instance.argocd.argocd.spec.instance_spec.subdomain}.cd.akuity.cloud:443"
-}
--- terraform/gke-akuity/modules/akuity/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "akuity_server_addr" {
-  value = "${akp_instance.argocd.argocd.spec.instance_spec.subdomain}.cd.akuity.cloud:443"
-}
